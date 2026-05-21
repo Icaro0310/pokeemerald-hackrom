32bf2cdf34390b90a049289f4f33e942350b9642
Icaro0310 <icarogalvao5@gmail.com>
2026-05-21 15:30:48 +0100

    Initial commit: pokeemerald base build

---
+++ include/types.h
@@ -0,0 +1,11 @@
+#ifndef GUARD_TYPES_H
+#define GUARD_TYPES_H
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned long u32;
+typedef unsigned long long u64;
+typedef signed char s8;
+typedef signed short s16;
+typedef signed long s32;
+typedef signed long long s64;
+#endif
